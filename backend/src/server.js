4b7c150b4aa3001c9796acd4e547ad56b1adda25
Xavier Zaidane Athaya <xavierzaidane@gmail.com>
2025-08-20 23:20:41 +0700

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,35 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./config/inngest.js"
 
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware()); 
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
     res.send("Hello World! 123")
 })
 
-console.log("mongo uri:", ENV.MONGO_URI);
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); 
+  }
+};
 
-app.listen(ENV.PORT, () => console.log("Server started on port:",ENV.PORT))
+startServer();
+
+export default app;
