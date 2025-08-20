5adcc29cd445d2bd0f4b0eaab58e17c9c7dea093
Xavier Zaidane Athaya <xavierzaidane@gmail.com>
2025-08-20 23:25:38 +0700

    vercel.json added

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,7 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js"; // Import the User model
-import { deleteStreamUser} from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
